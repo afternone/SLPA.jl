d06470ea059b10decf382e813be16739f3e9c9ce
afternone <afternone@163.com>
2015-09-27 15:11:28 +0800

    v0.1

--- src/SLPA.jl
+++ src/SLPA.jl
@@ -1,5 +1,22 @@
 module SLPA
 
-# package code goes here
+using StatsBase
+using Graphs
+
+export
+    slpa,
+    post_processing!,
+    getgrp,
+    nmi,
+    msp2grp,
+    grp2msp,
+    readgrp,
+    readmsp,
+    writegrp,
+    writemsp
+
+include("slpafunc.jl")
+include("utils.jl")
+include("nmi")
 
 end # module
